881e11031be8c0399646b0d74cee991eea9360b8
Camila Ingaramo <camilaingaramo44@gmail.com>
2025-03-18 20:53:05 +0100

    creacion de server y handler inicial

---
+++ server.js
@@ -0,0 +1,10 @@
+const http = require('http')
+const handler = require('./handler')
+
+const server = http.createServer(handler) 
+
+
+server.listen (3000, () =>{
+    console.log("Servidor levantado")
+})
+ 
